57c7af7d4a0cd8885c76d0ff1ee363881c23c1a3
Albert Wang <albertyw@mit.edu>
2013-05-27 22:54:26 -0400

    Added page for most recent blog post

---
+++ most-recent-post.php
@@ -0,0 +1,11 @@
+<?php
+require('wp-load.php');
+$recent_posts = wp_get_recent_posts();
+$recent_post = $recent_posts[0];
+// print_r($recent_post);
+echo get_permalink($recent_post["ID"]);
+echo "\n";
+echo $recent_post["post_title"];
+echo "\n";
+echo $recent_post["post_date"];
+?>
