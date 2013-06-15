aa30e36d7367962fa9d4068f2f2f2a05764b785e
Albert Wang <albertyw@mit.edu>
2013-06-15 18:36:09 -0400

    Made deployment script send emails

--- statsonice_deploy.php
+++ statsonice_deploy.php
@@ -1,7 +1,27 @@
 <?php
-echo '<pre>';
-echo 'Starting Deployment';
-echo `/bin/bash /home/statsonice_production/scripts/deploy_production.sh`;
-echo `/bin/bash /home/statsonice_staging/scripts/deploy_staging.sh`;
-echo '</pre>';
+$production_deployment = `/bin/bash /home/statsonice_production/scripts/deploy_production.sh`;
+$staging_deployment = `/bin/bash /home/statsonice_staging/scripts/deploy_staging.sh`;
+
+$message +=  '<h1>';
+$message +=  "Production Deployment";
+$message += '</h1>';
+$message += '<pre>';
+$message +=  $production_deployment;
+$message += '</pre>';
+
+$message += '<h1>';
+$message += "Staging Deployment";
+$message += '</h1>';
+$message += '<pre>';
+$message +=  $staging_deployment;
+$message += '</pre>';
+
+$to = 'albertyw@statsonice.com, curranoi@statsonice.com, lekha@statsonice.com';
+$subject = 'Deployment Log';
+$headers = "From: " . strip_tags('statsonice-deploy@statsonice.com') . "\r\n";
+$headers .= "MIME-Version: 1.0\r\n";
+$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+
+mail($to, $subject, $message, $headers);
+
 ?>
