03cdbeef0c0c761cf1da3cd6ef1ff89bab1c965f
Albert Wang <albertyw@mit.edu>
2013-06-15 19:12:46 -0400

    fix bug with php concatenating strings

--- statsonice_deploy.php
+++ statsonice_deploy.php
@@ -2,19 +2,21 @@
 $production_deployment = `/bin/bash /home/statsonice_production/scripts/deploy_production.sh`;
 $staging_deployment = `/bin/bash /home/statsonice_staging/scripts/deploy_staging.sh`;
 
-$message +=  '<h1>';
-$message +=  "Production Deployment";
-$message += '</h1>';
-$message += '<pre>';
-$message +=  $production_deployment;
-$message += '</pre>';
+$message =  '<h1>';
+$message .=  "Production Deployment";
+$message .= '</h1>';
+$message .= '<pre>';
+$message .=  $production_deployment;
+$message .= '</pre>';
 
-$message += '<h1>';
-$message += "Staging Deployment";
-$message += '</h1>';
-$message += '<pre>';
-$message +=  $staging_deployment;
-$message += '</pre>';
+$message .= '<h1>';
+$message .= "Staging Deployment";
+$message .= '</h1>';
+$message .= '<pre>';
+$message .=  $staging_deployment;
+$message .= '</pre>';
+echo $message;
+echo '<br />';
 
 $to = 'albertyw@statsonice.com, curranoi@statsonice.com, lekha@statsonice.com';
 $subject = 'Deployment Log';
@@ -23,5 +25,6 @@ $headers .= "MIME-Version: 1.0\r\n";
 $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
 mail($to, $subject, $message, $headers);
+echo '<b>Mail Sent</b>';
 
 ?>
