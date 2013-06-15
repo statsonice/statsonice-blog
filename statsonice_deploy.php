9da598086cef5b6d8f6443c4f6872706c016f2fd
Albert Wang <albertyw@mit.edu>
2013-06-15 02:42:10 -0400

    Added staging deploy script

--- statsonice_deploy.php
+++ statsonice_deploy.php
@@ -1,5 +1,5 @@
 <?php
 echo 'Starting Deployment';
 echo `/bin/bash /home/statsonice_production/scripts/deploy_production.sh`;
-#echo '/bin/bash /home/statsonice_staging/scripts/deploy_staging.sh';
+echo '/bin/bash /home/statsonice_staging/scripts/deploy_staging.sh';
 ?>
