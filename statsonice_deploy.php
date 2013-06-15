6cedd166646d0339a669c156fe547f90d8e1e5e9
Albert Wang <albertyw@mit.edu>
2013-06-15 03:16:12 -0400

    Oops

--- statsonice_deploy.php
+++ statsonice_deploy.php
@@ -1,5 +1,7 @@
 <?php
+echo '<pre>';
 echo 'Starting Deployment';
 echo `/bin/bash /home/statsonice_production/scripts/deploy_production.sh`;
-echo '/bin/bash /home/statsonice_staging/scripts/deploy_staging.sh';
+echo `/bin/bash /home/statsonice_staging/scripts/deploy_staging.sh`;
+echo '</pre>';
 ?>
