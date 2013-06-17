35e2c94736ae1ec58a6a7e15fc24cc94e788a775
Albert Wang <albertyw@mit.edu>
2013-06-16 21:40:02 -0400

    Made deployment emails not send when there are no deployments

--- statsonice_deploy.php
+++ statsonice_deploy.php
@@ -2,29 +2,37 @@
 $production_deployment = `/bin/bash /home/statsonice_production/scripts/deploy.sh production`;
 $staging_deployment = `/bin/bash /home/statsonice_staging/scripts/deploy.sh master`;
 
-$message =  '<h1>';
-$message .=  "Production Deployment";
-$message .= '</h1>';
-$message .= '<pre>';
-$message .=  $production_deployment;
-$message .= '</pre>';
+$message = '';
+if(strpos($production_deployment, "No new code to deploy") === False){
+    $message .=  '<h1>';
+    $message .=  "Production Deployment";
+    $message .= '</h1>';
+    $message .= '<pre>';
+    $message .=  $production_deployment;
+    $message .= '</pre>';
+}
 
-$message .= '<h1>';
-$message .= "Staging Deployment";
-$message .= '</h1>';
-$message .= '<pre>';
-$message .=  $staging_deployment;
-$message .= '</pre>';
-echo $message;
-echo '<br />';
+if(strpos($staging_deployment, "No new code to deploy") === False){
+    $message .= '<h1>';
+    $message .= "Staging Deployment";
+    $message .= '</h1>';
+    $message .= '<pre>';
+    $message .=  $staging_deployment;
+    $message .= '</pre>';
+    echo $message;
+    echo '<br />';
+}
 
-$to = 'albertyw@statsonice.com, curranoi@statsonice.com, lekha@statsonice.com';
-$subject = 'Deployment Log';
-$headers = "From: " . strip_tags('statsonice-deploy@statsonice.com') . "\r\n";
-$headers .= "MIME-Version: 1.0\r\n";
-$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
-
-mail($to, $subject, $message, $headers);
-echo '<b>Mail Sent</b>';
-
+if($message != ''){
+    $to = 'admin@statsonice.com';
+    $subject = 'Deployment Log';
+    $headers = "From: " . strip_tags('statsonice-deploy@statsonice.com') . "\r\n";
+    $headers .= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+    
+    mail($to, $subject, $message, $headers);
+    echo '<b>Mail Sent</b>';
+}else{
+    echo 'No deployments done';
+}
 ?>
