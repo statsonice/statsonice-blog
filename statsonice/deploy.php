890eba4c1616d414b03afdf0393f62b19054b139
Albert Wang <albertyw@mit.edu>
2013-09-21 17:12:21 -0400

    Updated deploy to not use pre

--- statsonice/deploy.php
+++ statsonice/deploy.php
@@ -27,31 +27,21 @@ $staging_deployment = `/bin/bash /home/statsonice_staging/scripts/deploy.sh mast
 
 $message = '';
 if(strpos($production_deployment, "No new code to deploy") === False){
-    $message .=  '<h1>';
-    $message .=  "Production Deployment";
-    $message .= '</h1>';
-    $message .= '<pre>';
+    $message .=  "Production Deployment\n\n";
     $message .=  $production_deployment;
-    $message .= '</pre>';
 }
 
 if(strpos($staging_deployment, "No new code to deploy") === False){
-    $message .= '<h1>';
-    $message .= "Staging Deployment";
-    $message .= '</h1>';
-    $message .= '<pre>';
+    $message .= "Staging Deployment\n\n";
     $message .=  $staging_deployment;
-    $message .= '</pre>';
-    echo $message;
-    echo '<br />';
 }
+echo $message;
+echo '<br />';
 
 if($message != ''){
     $to = 'admin@statsonice.com';
     $subject = 'Deployment Log';
     $headers = "From: " . strip_tags('statsonice-deploy@statsonice.com') . "\r\n";
-    $headers .= "MIME-Version: 1.0\r\n";
-    $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
     
     mail($to, $subject, $message, $headers);
     echo '<b>Mail Sent</b>';
